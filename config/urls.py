d4adf121c0ca9488045788ad53c2611735571fd5
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-16 13:05:34 +0900

    #Urls, Views and Templates

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from tweets.views import tweet_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', tweet_list, name='tweet_list'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
+
+def tweet_list(request):
+    tweets = Tweet.objects.all()
+    return render(request, 'tweets/tweet_list.html', {'tweets': tweets})
